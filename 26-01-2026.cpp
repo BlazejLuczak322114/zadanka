50b0d9b460e1bfeda85cde5f5314c32f6aa1cda8
Błażej Łuczak <bl322114@student.polsl.pl>
2026-01-26 12:47:15 +0100

    Create 26-01-2026.cpp

---
+++ 26-01-2026.cpp
@@ -0,0 +1,66 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <memory>
+
+using namespace std;
+
+class Product {
+public:
+    string nazwa;
+    string typ;
+    int ilosc;
+    double cena;
+    Product(string NowaNazwa, string NowyTyp, int NowaIlosc, double NowaCena) {
+        nazwa = NowaNazwa;
+        typ = NowyTyp;
+        ilosc = NowaIlosc;
+        cena = NowaCena;
+    }
+    void wypisz() {
+        cout << nazwa << " " << typ << " " << ilosc << " " << cena << endl;
+    }
+private:
+    shared_ptr<Product> next; //zrobic coś z tym w domu z shared, unique i weak ptr
+    shared_ptr<Product> prev;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    fstream plik("magazyn.txt", ios::in);
+    if (!plik.good()) {
+        cout << "Plik not good" << endl;
+        return 0;
+    }
+    vector<Product> StanMagazynu;
+    string nazwa, typ;
+    int ilosc;
+    double cena;
+    while (plik>>nazwa >> typ >> ilosc >> cena) {
+        StanMagazynu.push_back(Product(nazwa, typ, ilosc, cena));
+    }
+    bool dziala = true;
+    std::string wejscie = "";
+    while (dziala) {
+        cout << "Wybierz produkt do wypisania(karta_graficzna,dysk,monitor,procesor,obudowa,plyta)" << endl;
+        cin >> wejscie;
+        for (int i = 0; i < StanMagazynu.size(); i++) {
+            if (StanMagazynu[i].typ == wejscie) {
+                StanMagazynu[i].wypisz();
+            }
+        }
+        //cout << "Posortowac wg ceny?(rosnaco/malejaco)" << endl;
+        cout << "napisz 'stop' by stop" << endl;
+        cin >> wejscie;
+        if (wejscie == "stop") {
+            dziala = false;
+        }
+        
+    }
+    
+}
