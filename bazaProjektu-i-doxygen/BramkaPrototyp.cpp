03520835a07c6a8e76da9eb35b20a131499215b0
Błażej Łuczak <bl322114@student.polsl.pl>
2026-01-26 04:50:53 +0100

    Add files via upload

---
+++ bazaProjektu-i-doxygen/BramkaPrototyp.cpp
@@ -0,0 +1,37 @@
+﻿/**
+ * @file BramkaPrototyp.cpp
+ * @brief Implementacja klasy BramkaPrototyp i funkcji obliczania bramek logicznych.
+ */
+
+#include "BramkaPrototyp.h"
+
+/**
+ * @brief Konstruktor klasy BramkaPrototyp.
+ *
+ * Ustawia wszystkie pola obiektu bramki oraz inicjalizuje wartości
+ * (value = -1 oznacza brak obliczonego wyniku).
+ */
+BramkaPrototyp::BramkaPrototyp(std::string typA, std::string in1, std::string in2, std::string wynikA, int czasA) { //konstruktor
+    typ = typA;
+    wejscie1 = in1;
+    wejscie2 = in2;
+    wynik = wynikA;
+    czas_propagacji = czasA;
+    value = -1;
+    czas_skumulowany = 0;
+}
+
+/**
+ * @brief Implementacja obliczania wartości bramki logicznej.
+ *
+ * Na podstawie typu bramki (np. AND, OR, NAND) wykonywana jest odpowiednia
+ * operacja logiczna na wejściach.
+ */
+int obliczBramke(BramkaPrototyp &br, int val1, int val2) { 
+    if (br.typ == "NAND") return !(val1 & val2);
+    else if (br.typ == "AND") return val1 & val2;
+    else if (br.typ == "OR")  return val1 | val2;
+    else if (br.typ == "NOR") return !(val1 | val2);
+    else if (br.typ == "XOR") return val1 ^ val2;
+    return 0;
+}
---
+++ bazaProjektu-i-doxygen/BramkaPrototyp.h
@@ -0,0 +1,74 @@
+﻿/**
+ * @file BramkaPrototyp.h
+ * @brief Deklaracja klasy BramkaPrototyp oraz funkcji obsługujących bramki logiczne.
+ *
+ * Plik zawiera definicję klasy reprezentującej bramkę logiczną
+ * używaną w symulacji układu logicznego wraz z deklaracją
+ * funkcji obliczającej wartość wyjścia bramki.
+ */
+
+#ifndef BRAMKAPROTOTYP_H
+#define BRAMKAPROTOTYP_H
+
+#include <string>
+
+/**
+ * @class BramkaPrototyp
+ * @brief Reprezentuje pojedynczą bramkę logiczną w symulowanym układzie.
+ *
+ * Klasa przechowuje informacje o typie bramki logicznej, nazwach sygnałów
+ * wejściowych i wyjściowych oraz danych potrzebnych do symulacji
+ * sygnału w czasie.
+ */
+class BramkaPrototyp {
+public:
+    /// Typ bramki logicznej (np. AND, OR, NAND, XOR)
+    std::string typ;
+
+    /// Nazwa pierwszego sygnału wejściowego
+    std::string wejscie1;
+
+    /// Nazwa drugiego sygnału wejściowego
+    std::string wejscie2;
+
+    /// Nazwa sygnału wyjściowego bramki
+    std::string wynik;
+
+    /// Czas propagacji sygnału przez bramkę
+    int czas_propagacji;
+
+    /// Aktualna wartość logiczna wyjścia (0, 1 lub -1 gdy nieobliczona)
+    int value;
+
+    /// Skumulowany czas propagacji sygnału do tej bramki
+    int czas_skumulowany;
+
+    /**
+     * @brief Konstruktor klasy BramkaPrototyp.
+     *
+     * Inicjalizuje bramkę logiczną na podstawie podanych danych 
+     *      
+     * @param typA typ bramki logicznej
+     * @param in1 nazwa pierwszego wejścia
+     * @param in2 nazwa drugiego wejścia
+     * @param wynikA nazwa sygnału wyjściowego
+     * @param czasA czas propagacji bramki
+     */
+    BramkaPrototyp(std::string typA, std::string in1, std::string in2, std::string wynikA, int czasA); 
+};
+
+/**
+ * @brief Oblicza wartość logiczną wyjścia bramki.
+ *
+ * Funkcja na podstawie typu bramki zapisanej w obiekcie BramkaPrototyp
+ * oblicza wynik operacji logicznej dla podanych wartości wejściowych.
+ *
+ * @param br referencja do obiektu bramki
+ * @param val1 wartość pierwszego wejścia (0 lub 1)
+ * @param val2 wartość drugiego wejścia (0 lub 1)
+ * @return wynik operacji logicznej (0 lub 1)
+ */
+int obliczBramke(BramkaPrototyp &br, int val1, int val2); 
+
+#endif // BRAMKAPROTOTYP_H
+
